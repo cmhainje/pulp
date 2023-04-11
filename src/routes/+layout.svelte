8eb74981e8a7e384c1b961b8a5603350de4b9591
Connor Hainje <cmhainje@gmail.com>
2023-04-11 10:23:15 -0400

    add a home page and redirect between index and home depending on auth state

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,3 @@
+<svelte:head><title>pulp</title></svelte:head>
+
+<slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>you're not logged in!</p>
+
+<form method="POST" action="/login">
+  <input type="text" name="username" id="username" placeholder="username">
+  <input type="password" name="password" id="password" placeholder="password">
+  <button type="submit">log in</button>
+</form>
+
+<p>don't have an account?</p>
+
+<form method="POST" action="/register">
+  <input type="text" name="username" id="username" placeholder="username">
+  <input type="password" name="password" id="password" placeholder="password">
+  <button type="submit">register</button>
+</form>
